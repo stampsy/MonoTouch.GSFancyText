3f2322f23913ce703678a7fb8575f4399e4fc817
Kuzya <alexkuz@gmail.com>
2012-12-27 12:54:52 +0400

    add files

---
+++ Extra.cs
@@ -0,0 +1,16 @@
+using System;
+using MonoTouch.UIKit;
+using MonoTouch.Foundation;
+
+namespace GrannySmith
+{
+    public partial class GSFancyTextView : UIView
+    {
+    }
+
+    public partial class GSFancyText : NSObject
+    {
+
+    }
+}
+
